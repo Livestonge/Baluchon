243eaf98a125837e340dbbe508d3fb3b978917ab
Awaleh Hassan <awaleh@squadeasy.com>
2022-06-05 14:10:43 +0200

    fixed a bug and deleted test for viewcontrollers

--- BaluchonTests/ViewModelTests/Weather/WeatherViewModelTest.swift
+++ BaluchonTests/ViewModelTests/Weather/WeatherViewModelTest.swift
@@ -13,37 +13,39 @@ class WeatherViewModelTest: XCTestCase {
     
     var viewModel: WeatherManager?
   
-    override func setUpWithError() throws {
+    override func setUp() {
       viewModel = WeatherManager(initialeState: .init(),
                                    iniatialAction: .none,
                                    service: MockWeatherRestApi())
-      try super.setUpWithError()
+      super.setUp()
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         viewModel = nil
-        try super.tearDownWithError()
+        super.tearDown()
+
     }
   
   func testActionChanges() {
     let expectation = XCTestExpectation(description: "City name")
-    var receivedCityName: String = ""
+    var weatherBaseState: WeatherBaseState?
 //  When
     let cityName = "Oslo"
     viewModel?.action = .didProvideCity(cityName)
 //  Then
     _ = self.viewModel?.getStateChanges()
-                       .subscribe(onNext: { state in
-                         receivedCityName = state.localWeather.locationName
+                        .debounce(.microseconds(20), scheduler: MainScheduler.instance)
+                        .subscribe(onNext: { state in
+                         weatherBaseState = state
                          expectation.fulfill()
                        })
     wait(for: [expectation], timeout: 2)
-    XCTAssertEqual(receivedCityName, "Paris")
+    XCTAssertEqual(weatherBaseState?.localWeather.locationName, "Paris")
   }
   
   func testLocationChanges() throws {
 //    When
-    let expectation = XCTestExpectation(description: "")
+    let expectation = XCTestExpectation(description: "") 
     let location = Location(latitude: 23.433, longitude: 45.456)
     viewModel?.action = .didUpdateUser(location)
     var receivedCityName: String = ""
